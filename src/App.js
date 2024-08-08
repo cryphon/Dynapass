f6320c63c3d2cf3e6ea66b8050f5134b86ce58e3
cryphon <yvanroes@gmail.com>
2024-08-08 18:20:34 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
+import React from 'react';
+import { Button, CssBaseline, Container, Typography } from '@mui/material'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
 import logo from './logo.svg';
 import './App.css';
 
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#1976d2',
+        },
+    },
+});
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Container>
+            <Typography variant="h4" component="h1" gutterBottom>
+                My Chrome extension
+            </Typography>
+            <Button variant="contained" color="primary">
+                Click Me
+            </Button>
+        </Container>
+    </ThemeProvider>
   );
 }
 
