625161326f5fea6c29a0fc4a3c438371f90dea40
cryphon <yvanroes@gmail.com>
2024-08-10 14:18:44 +0000

    fixed positioning of the popup

--- src/content/content.js
+++ src/content/content.js
@@ -5,7 +5,7 @@ document.addEventListener('mouseup', function () {
     
     const rect = window.getSelection().getRangeAt(0).getBoundingClientRect();
     const coords = {
-      top: rect.top + window.scrollY - 50, // Adjust 50px above the selection
+      top: rect.top + window.scrollY - 125, // Adjust 50px above the selection
       left: rect.left + window.scrollX,
     };
 
@@ -19,12 +19,13 @@ document.addEventListener('mouseup', function () {
     popupContainer.style.position = 'absolute';
     popupContainer.style.top = `${coords.top}px`;
     popupContainer.style.left = `${coords.left}px`;
-    popupContainer.style.backgroundColor = 'white';
-    popupContainer.style.border = '1px solid #ccc';
+    popupContainer.style.backgroundColor = '#f5f5f5';
+    popupContainer.style.border = '2px solid #d9e1e6';
     popupContainer.style.padding = '10px';
     popupContainer.style.zIndex = 10000;
-    popupContainer.style.boxShadow = '0 2px 10px rgba(0,0,0,0.1)';
+    popupContainer.style.boxShadow = '0 4px 10px rgba(0,0,0,0.1)';
     popupContainer.style.width = '300px';
+    popupContainer.style.borderRadius = '10px';
 
     const popupRoot = document.createElement('div');
     popupRoot.id = 'popup-root';
