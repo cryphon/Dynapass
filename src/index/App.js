425ac2ca063a8320b8a7b37612b9c3b2588b1d00
cryphon <yvanroes@gmail.com>
2024-08-19 00:04:37 +0000

    mainPanel: added speedDial menu

--- src/index/App.js
+++ src/index/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { useTheme, ThemeProvider } from '../theme/ThemeProvider';
-import { CssBaseline, Button, Typography } from '@mui/material';
+import { CssBaseline, Button, Typography, Box, Backdrop, SpeedDial, SpeedDialIcon, SpeedDialAction } from '@mui/material';
+import { SettingsIcon } from '@mui/icons-material';
+
+const actions = [
+    { icon: <SettingsIcon />, name: 'Settings' },
+]
+
+export default function SpeedDialTooltipOpen() {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+}
 
 const App = () => {
     const { theme, toggleTheme } = useTheme();
@@ -13,6 +24,24 @@ const App = () => {
                 height: 500,
             }}>
             <CssBaseline />
+            <Backdrop open={open} />
+            <SpeedDial
+                ariaLabel="SpeedDial menu"
+                sx={{ position: 'absolute', bottom: 16, right: 16 }}
+                icon={<SpeedDialIcon />}
+                onClose={handleClose}
+                onOpen={handleOpen}
+                open={open} >
+                    {actions.map((action) => (
+                        <SpeedDialAction
+                            key={action.name}
+                            icon={action.icon}
+                            tooltipTitle={action.name}
+                            toolTipOpen
+                            onClick={handleClose}
+                        />
+                    ))}
+            </SpeedDial>
             <Typography variant="h4" gutterBottom>
                 {theme.charAt(0).toUpperCase() + theme.slice(1)} Theme
             </Typography>
